1c0624149379dea35d687ab498e98154705b9e4e
Alejandro Druetta <aledruetta@gmail.com>
2017-05-28 12:39:36 -0300

    yarn run libs

--- libs/normalize.js
+++ libs/normalize.js
@@ -1,1 +1,36 @@
-node_modules/normalize/lib/normalize.js
+/**
+ * Module dependencies.
+ */
+
+var stylus = require('stylus');
+
+/**
+ * Expose callback function.
+ */
+
+exports = module.exports = plugin;
+
+/**
+ * Library version.
+ */
+
+exports.version = '0.2.0';
+
+/**
+ * Stylus path.
+ */
+
+exports.path = __dirname;
+
+/**
+ * Return the plugin callback for stylus.
+ *
+ * @return {Function}
+ * @api public
+ */
+
+function plugin() {
+  return function(style) {
+    style.include(__dirname);
+  };
+}
